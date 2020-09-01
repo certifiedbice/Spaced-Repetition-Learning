5cbbcfcd216d840f0e365d191aed099e22f61f23
jenthura <joshuasth@gmail.com>
2020-09-01 10:23:33 -0500

    added lang-service object with get fetch

--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,13 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import LanguageService from '../../services/lang-service.js';
 
 class DashboardRoute extends Component {
   render() {
-    return (
-      <section>
-        implement and style me
-      </section>
-    );
+    return <section>implement and style me</section>;
   }
 }
 
-export default DashboardRoute
+export default DashboardRoute;
