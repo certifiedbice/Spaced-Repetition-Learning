edbe72c35b9636e5b80e63057649e414b2fd692c
jenthura <joshuasth@gmail.com>
2020-09-02 17:57:38 -0500

    changed postgrator migration variables from DB to DATABASE

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 
 module.exports = {
-  "migrationDirectory": "migrations",
-  "driver": "pg",
-  "host": process.env.MIGRATION_DB_HOST,
-  "port": process.env.MIGRATION_DB_PORT,
-  "connectionString":(process.env.NODE_ENV==='test')
-		? process.env.MIGRATION_TEST_DB_NAME
-		: process.env.MIGRATION_DB_NAME,
-  "username": process.env.MIGRATION_DB_USER,
-  "password": process.env.MIGRATION_DB_PASS
-}
+  'migrationDirectory': 'migrations',
+  'driver': 'pg',
+  'host': process.env.MIGRATION_DATABASE_HOST,
+  'port': process.env.MIGRATION_DATABASE_PORT,
+  'connectionString':
+    process.env.NODE_ENV === 'test'
+      ? process.env.MIGRATION_TEST_DATABASE_NAME
+      : process.env.MIGRATION_DATABASE_NAME,
+  'username': process.env.MIGRATION_DATABASE_USER,
+  'password': process.env.MIGRATION_DATABASE_PASS,
+};
