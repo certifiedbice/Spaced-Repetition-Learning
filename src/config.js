6e945280df517fa48a11f33d16b5b78f3d912466
jenthura <joshuasth@gmail.com>
2020-09-02 18:37:07 -0500

    added API_ENDPOINT logic for production

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
+  API_ENDPOINT:
+    process.env.REACT_APP_ENV === 'development'
+      ? 'https://warm-dawn-94119.herokuapp.com/api'
+      : 'http://localhost:8000',
   TOKEN_KEY: 'spaced-repetition-client-auth-token',
-}
+};
