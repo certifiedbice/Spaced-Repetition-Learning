5d57f57c0ca5acfb7c23c5a11fea7f8bf55b7922
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-09-02 21:44:57 -0600

    Added conditional for API_ENDPOINT env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,7 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api',
-  TOKEN_KEY: 'spaced-repetition-client-auth-token',
+	NODE_ENV:process.env.NODE_ENV||'development',
+  	TOKEN_KEY: 'spaced-repetition-client-auth-token',
+	API_ENDPOINT:(process.env.NODE_ENV==='development')
+		? 'http://localhost:8000/api'
+		: 'https://warm-dawn-94119.herokuapp.com/api'
 }
--- src/routes/DashboardRoute/DashboardRoute.js
+++ src/routes/DashboardRoute/DashboardRoute.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import {Redirect} from "react-router-dom";
 import LanguageService from '../../services/lang-service.js';
 import UserContext from '../../contexts/UserContext';
 import Dashboard from '../../components/Dashboard/Dashboard';
@@ -10,12 +9,6 @@ class DashboardRoute extends Component {
 
   async componentDidMount() {
     const dashboard = await LanguageService.getAllLanguageData()
-	console.log(dashboard)
-	// if(dashboard===401){
-		// this.context.setRedirect(true);
-	// }
-    // if(dashboard!==401)
-	// if(dashboard!==undefined)
 	this.context.setDashboard(dashboard);
   }
 
