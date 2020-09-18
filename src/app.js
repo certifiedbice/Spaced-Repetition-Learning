f9abdf38e20a2cfd0cadcf5ed9bc3789509e5264
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-09-18 13:56:38 -0600

    attempting to fix crashing 2

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const errorHandler = require('./middleware/error-handler')
-// const authRouter = require('./auth/auth-router')
+const authRouter = require('./auth/auth-router')
 // const languageRouter = require('./language/language-router')
 // const userRouter = require('./user/user-router')
 
@@ -16,7 +16,7 @@ app.use(morgan((NODE_ENV === 'production') ? 'tiny' : 'common', {
 app.use(cors())
 app.use(helmet())
 
-// app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter)
 // app.use('/api/language', languageRouter)
 // app.use('/api/user', userRouter)
 app.get('/',(req,res)=>{res.send('Spaced Repetition');});
