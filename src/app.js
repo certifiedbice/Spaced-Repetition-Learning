7cef16468a32d691ab575226be73cf40d403ba93
Bice <59291964+certifiedbice@users.noreply.github.com>
2020-09-18 13:59:42 -0600

    attempting to fix crashing 4

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const helmet = require('helmet')
 const { NODE_ENV } = require('./config')
 const errorHandler = require('./middleware/error-handler')
 const authRouter = require('./auth/auth-router')
-// const languageRouter = require('./language/language-router')
+const languageRouter = require('./language/language-router')
 const userRouter = require('./user/user-router')
 
 const app = express()
@@ -17,7 +17,7 @@ app.use(cors())
 app.use(helmet())
 
 app.use('/api/auth', authRouter)
-// app.use('/api/language', languageRouter)
+app.use('/api/language', languageRouter)
 app.use('/api/user', userRouter)
 app.get('/',(req,res)=>{res.send('Spaced Repetition');});
 app.use(errorHandler)
